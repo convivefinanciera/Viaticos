4b664e7ff418c0d6f8b15b39752e3182c5e98027
Convive Financiera <88561692+convivefinanciera@users.noreply.github.com>
2024-09-05 10:21:40 -0600

    Update recuperarPass.php

--- Controllers/recuperarPass.php
+++ Controllers/recuperarPass.php
@@ -44,7 +44,7 @@ function enviarCorreoVerificacion($token, $email) {
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = 1;				                      //Enable verbose debug output
+        $mail->SMTPDebug = 0;				                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'mail.convivefinanciera.com';           //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
